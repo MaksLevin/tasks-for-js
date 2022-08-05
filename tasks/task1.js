233d162299128f234f158a9bbca4dd1714542b29
Maksim Levin <maksim.levin@hiqo-solutions.com>
2022-08-05 13:13:17 +0300

    completed Task-1

---
+++ tasks/task1.js
@@ -0,0 +1,20 @@
+function summ() {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i += 1) {
+    if (isNaN(arguments[i])) {
+      continue;
+    } else {
+      result += Number(arguments[i]);
+    }
+  }
+  return result;
+}
+
+summ(1, "2", "3", 5, "abc");
+
+// second option
+
+const sum = (...arr) =>
+  arr.filter(Number).reduce((a, c) => (a += Number(c)), 0);
+
+sum(1, "2", "3", 5, "abc");
